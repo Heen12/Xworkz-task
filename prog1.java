559980d605665691a6dad3c60530462a8eab63f2
Heena <heeena067@gmail.com>
2024-09-27 09:35:38 +0530

    Variables

---
+++ prog1.java
@@ -0,0 +1,25 @@
+public class prog1{
+    public static void main(String[] args) {
+               byte age = 10;
+        
+                byte tempreature= -5;
+                byte heightInInches = 127; 
+                byte speed = -128;         
+                byte score = 0;
+        
+                byte maxLimit = 25;
+        
+                byte minValue = -45;
+        
+               byte dayOfMonth = 100;
+
+                System.out.println("age: " + age);
+                System.out.println("tempreature: " + tempreature);
+                System.out.println("heightInInches: " + heightInInches);
+                System.out.println("speed : " + speed );
+                System.out.println("score: " + score);
+                System.out.println("maxLimit: " + maxLimit);
+                System.out.println("minValue: " +minValue);
+                System.out.println("dayOfMonth: " + dayOfMonth);
+    }
+}
